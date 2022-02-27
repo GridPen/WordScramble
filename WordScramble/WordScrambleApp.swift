f6f1c752cc6b592bf213b7ccc5b73bbaad51ac73
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-27 14:05:05 +0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Debashish Mondal on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
