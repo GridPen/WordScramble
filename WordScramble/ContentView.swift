69eedf55e2898b171ff0a9d1105552b5d6a275f9
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-03-06 19:23:00 +0600

    Function Added

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rootWord = ""
+    @State private var usedWord = [String]()
+    @State private var newWord = ""
+    
     var body: some View {
-        List{
-            ForEach(0...10, id: \.self) {
-                Text("Dynamic text \($0)")
+        NavigationView{
+            List{
+                Section{
+                    TextField("Your word", text: $newWord)
+                }
+                
+                Section{
+                    ForEach(usedWord, id: \.self) { word in
+                        Text(word)
+                    }
+                }
             }
+            .navigationTitle(rootWord)
         }
     }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
