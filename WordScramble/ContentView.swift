7bf485d74063f64ed21e61a2474446d973fdb10e
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-03-09 01:39:57 +0600

    Done!

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rootWord = ""
-    @State private var usedWord = [String]()
+    @State private var usedWords = [String]()
     @State private var newWord = ""
+    
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    
     
     var body: some View {
         NavigationView{
@@ -21,7 +28,7 @@ struct ContentView: View {
                 }
                 
                 Section{
-                    ForEach(usedWord, id: \.self) { word in
+                    ForEach(usedWords, id: \.self) { word in
                         HStack{
                             Image(systemName: "\(word.count).circle")
                             Text(word)
@@ -30,19 +37,42 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
-            .onSubmit {
-                addNewWord()
+            .onSubmit (addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
             }
         }
     }
     
+    
+    
+    
     func addNewWord() {
-        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        let answer = newWord.lowercased().trimmingCharacters(in : .whitespacesAndNewlines)
         guard answer.count > 0 else { return }
         
         //extra validation came to make here
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        
         withAnimation{
-                usedWord.insert(answer, at: 0)
+                usedWords.insert(answer, at: 0)
         }
         newWord = ""
     }
@@ -66,6 +96,36 @@ struct ContentView: View {
         // If were are *here* then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            }else {
+                return false
+            }
+            
+        }
+        return true
+    }
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
 
 
