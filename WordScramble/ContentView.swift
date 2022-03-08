005b7f2739beceabe25acf054952e6c7b363a31a
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-03-08 19:09:07 +0600

    New Function

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -16,12 +16,16 @@ struct ContentView: View {
         NavigationView{
             List{
                 Section{
-                    TextField("Your word", text: $newWord)
+                    TextField("Your word here", text: $newWord)
+                        .autocapitalization(.none)
                 }
                 
                 Section{
                     ForEach(usedWord, id: \.self) { word in
-                        Text(word)
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
                 }
             }
@@ -37,8 +41,30 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
         
         //extra validation came to make here
-        usedWord.insert(answer, at: 0)
+        withAnimation{
+                usedWord.insert(answer, at: 0)
+        }
         newWord = ""
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
